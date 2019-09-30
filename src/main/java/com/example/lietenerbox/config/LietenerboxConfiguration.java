58580675a902500cc05c5411bd479e62d9ef5ca2
kimjinwook <cicili0131@gmail.com>
2019-10-01 01:04:16 +0900

    webmvcconfigurer 상속 추가

--- src/main/java/com/example/lietenerbox/config/LietenerboxConfiguration.java
+++ src/main/java/com/example/lietenerbox/config/LietenerboxConfiguration.java
@@ -3,8 +3,10 @@ package com.example.lietenerbox.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableJpaAuditing
-public class LietenerboxConfiguration {
+public class LietenerboxConfiguration implements WebMvcConfigurer {
+
 }
