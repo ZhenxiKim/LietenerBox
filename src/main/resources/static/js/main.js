2168acde1b0cf96c11cb5f707627c4e20831e58e
8055kjh <8055kjh@gmail.com>
2019-11-05 18:54:19 +0900

    async 추가

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,19 +1,33 @@
-$(document).ready(function(){
-    ajaxfunction();
-});
+// $(document).ready(function(){
+//     ajaxfunction();
+// });
+//
+// function ajaxfunction(){
+//
+//     $.ajax({
+//         method:"GET",
+//         contentType:"application/json",
+//         url:"localhost:8080/members/1",
+//         dataType:'JSON',
+//         success: function(data){
+//             alert(data)
+//         },
+//         error: function(data){
+//             alert("error")
+//         }
+//     });
+// }
 
-function ajaxfunction(){
+(async () =>{
+    const response = await fetch(url);
+    const result = await response.json();
 
-    $.ajax({
-        method:"GET",
-        contentType:"application/json",
-        url:"localhost:8080/members/1",
-        dataType:'JSON',
-        success: function(data){
-            alert(data)
-        },
-        error: function(data){
-            alert("error")
-        }
-    });
+    const element - document.getElementById('app');
+    element.innerHTML = `
+    ${result.map(memberInfo => `
+        ${memberInfo.
+    
+    
+    `)}
+    `
 }
